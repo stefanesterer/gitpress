804ee22a5de4bd8b6bf53fe6fce45bfff0802331
Stefan Esterer <stefan.esterer@gmail.com>
2016-09-17 17:49:26 +0200

    * extend pushnotification pojo

---
+++ src/main/java/net/derstefon/gitpress/github/Commit.java
@@ -0,0 +1,13 @@
+package net.derstefon.gitpress.github;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Commit {
+
+	private Collection<String> modified = new ArrayList<>();
+
+	public Collection<String> getModified() {
+		return modified;
+	}
+}
--- src/main/java/net/derstefon/gitpress/github/PushNotification.java
+++ src/main/java/net/derstefon/gitpress/github/PushNotification.java
@@ -1,8 +1,12 @@
 package net.derstefon.gitpress.github;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class PushNotification {
 
 	private String ref;
+	private Collection<Commit> commits = new ArrayList<>();
 
 	public String getRef() {
 		return ref;
@@ -12,10 +16,13 @@ public class PushNotification {
 		this.ref = ref;
 	}
 
+	public Collection<Commit> getCommits() {
+		return commits;
+	}
+	
 	@Override
 	public String toString() {
 		return "PushNotification [ref=" + ref + "]";
 	}
-	
-	
+
 }
